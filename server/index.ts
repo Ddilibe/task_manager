cef2e58defc03f2e31b1fd5d211f00e7734b02f4
Ddilibe <franklinfidelugwuowo@gmail.com>
2025-07-19 08:37:00 +0100

    [Chore]: Added cors

--- server/index.ts
+++ server/index.ts
@@ -14,7 +14,9 @@ if (!JWT_SECRET) {
     process.exit(1);
 }
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://offpistataskmanager.netlify.app',
+}));
 app.use(express.json());
 app.use('/api', router);
 
